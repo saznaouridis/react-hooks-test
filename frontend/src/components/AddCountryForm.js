0723aa0513c64bfb2020550c674ab784cc371360
saznaouridis <saznaouridis@knowledge.gr>
2020-09-11 13:00:28 +0300

    materialUI

--- frontend/src/components/AddCountryForm.js
+++ frontend/src/components/AddCountryForm.js
@@ -1,18 +1,6 @@
 import React, { Fragment, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-	  '& > *': {
-		margin: theme.spacing(1),
-	  },
-	},
-  }));
 const AddCountryForm = props => {
-	const classes = useStyles();
 	const initialState = { }
 	const [ country, setCountry ] = useState(initialState)
 	const handleInputChange = e => {
@@ -55,9 +43,7 @@ const AddCountryForm = props => {
 				<input type="text" name="capital" value={country.capital} onChange={handleInputChange} />
 			
 			<p>
-				<IconButton color="primary" aria-label="add to shopping cart">
-					<AddShoppingCartIcon />
-					</IconButton>
+				<button className="btn btn-success">Add</button>
 			</p>
 		</form>
 		</Fragment>
