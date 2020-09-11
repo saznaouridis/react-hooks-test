50bef0a1909a21e4de353e8a2de273cfee3da591
saznaouridis <saznaouridis@knowledge.gr>
2020-09-11 13:18:46 +0300

    materialUI

--- frontend/src/components/AddCountryForm.js
+++ frontend/src/components/AddCountryForm.js
@@ -1,7 +1,19 @@
 import React, { Fragment, useState } from 'react'
 import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	margin: {
+	  margin: theme.spacing(1),
+	},
+	extendedIcon: {
+	  marginRight: theme.spacing(1),
+	},
+  }));
+  
 
 const AddCountryForm = props => {
+	const classes = useStyles();
 	const initialState = { }
 	const [ country, setCountry ] = useState(initialState)
 	const handleInputChange = e => {
