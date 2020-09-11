a48185370c1edf89b186befdbbfc96174b01e340
saznaouridis <saznaouridis@knowledge.gr>
2020-09-11 14:25:52 +0300

    materialUI

--- frontend/src/components/EditCountryForm.js
+++ frontend/src/components/EditCountryForm.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from "@material-ui/core/IconButton";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -58,10 +57,10 @@ const EditCountryForm = props => {
         <label for="capital">Capital</label>
         <input type="text" name="capital" value={country.capital} onChange={handleInputChange} />
       <p>
-        <button variant="contained" color="primary">Update Country</button>
-        <button variant="contained" color="primary" onClick={() => props.setEdit(false)} className="button muted-button">
+        <Button variant="contained" color="primary"> onclick ={e => {updateCountry(country.id, country)}}> Update Country</Button>
+        <Button variant="contained" color="primary" onClick={() => props.setEdit(false)} className="button muted-button">
         Cancel
-        </button>
+        </Button>
       </p>
     </form>
   )
