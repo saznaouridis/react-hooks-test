8d33d4db123482525679d91279667c26060ccab1
saznaouridis <saznaouridis@knowledge.gr>
2020-09-11 15:32:59 +0300

    materialUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,9 +122,9 @@ const App = () => {
 		const classes = useStyles();
 		return (
 			<div className={classes.root}>
-				<h1 id = "hed2">Home;
+				<h1 id = "hed2">Home
 				<HomeIcon alignitems="center" justify="center"
-				 style={{ fontSize: 40 }} />
+				 style={{ fontSize: 50 }} />
 				</h1>
 			</div>
 		)
