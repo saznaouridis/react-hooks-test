b6a14103ec4c8b55ef027111f7a8e5f1983d0c98
saznaouridis <saznaouridis@knowledge.gr>
2020-09-14 10:19:27 +0300

    materialUI

--- frontend/src/api_helpers.js
+++ frontend/src/api_helpers.js
@@ -13,6 +13,14 @@ export const updateCountry = async (id, updatedCountry) => {
     await axios.put(`/${id}`,updatedCountry);
     return
   }
-  export const getApiData = async() =>{
-  const {data} = await axios.get(`/countries`);
+  export const getApiData = () =>{
+    // const {data} = await axios.get(`/countries`);
+    let dataLocal = [
+      {capital: "Rome", id: 2, name: "Italy"},
+      {capital: "Budapest", id: 3, name: "Hungary"},
+      {capital: "Madrid", id: 4, name: "Spain"},
+      {capital: "Berlin", id: 5, name: "Germany"},
+      {capital: "asdas", id: 71, name: "S.Aznaouridis"},
+    ]
+    return dataLocal;
   }
