5fe5c10e213fd10b29bdeb066efeec46a9ed0855
saznaouridis <saznaouridis@knowledge.gr>
2020-09-14 14:44:25 +0300

    materialUI

--- frontend/src/components/EditCountryForm.js
+++ frontend/src/components/EditCountryForm.js
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { updateCountry } from '../api_helpers';
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  '& > svg': {
+		margin: theme.spacing(2),
+		TypographyStyle: {
+			color:"blue"
+		}
+	  },
+	},
+}));
+
+
 
 const EditCountryForm = (props) => {
+  const classes = useStyles();
   const [ country, setCountry ] = useState(props.curCountry)
   useEffect(
     () => {
@@ -34,10 +52,10 @@ const EditCountryForm = (props) => {
         <input placeholder="Enter Capital Name" type="text" name="capital" value={country.capital} onChange={handleInputChange} />
       <p>
         <div>
-        <button variant="contained" color="primary">Update Country</button> 
-        <button variant="contained" color="primary"onClick={() => props.setEdit(false)} className="button muted-button">
+        <Button type="submit" size="small" variant="contained" color="primary">Update Country</Button> 
+        <Button type="submit" size="small" variant="contained" color="primary"onClick={() => props.setEdit(false)} className="button muted-button">
         Cancel
-        </button>
+        </Button>
         </div>
       </p>
     </form>
