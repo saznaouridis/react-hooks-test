a5c717d715165c0748a8ba5ffed24090837426e3
saznaouridis <saznaouridis@knowledge.gr>
2020-09-14 11:38:45 +0300

    materialUI

--- frontend/src/components/PageAdd.js
+++ frontend/src/components/PageAdd.js
@@ -1,27 +1,63 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import EditCountryForm from './EditCountryForm';
 import AddCountryForm from './AddCountryForm';
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  '& > svg': {
+		margin: theme.spacing(2),
+		TypographyStyle: {
+			color:"blue"
+		}
+	  },
+	},
+}));
+
 
 const PageAdd = (props) => {
+   const classes = useStyles();
   return (
-    <div className="flex-row">
-      <div className="flex-large classleft">
+    <div className={classes.root}>
+     <Grid
+        container 
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
         {props.edit ? (
-          <Fragment>
-            <h2>Edit</h2>
+          <Grid item>
+            <Typography
+            align="left"
+            color="primary"
+            className={classes.TypographyStyle}
+            style={{ fontSize: 25  }}
+          >
+            Edit
+            </Typography>
             <EditCountryForm
               setEdit={props.setEdit}
               curCountry={props.curCountry}
             />
-          </Fragment>
+          </Grid>
         ) : (
-        <Fragment>
-          <h2>Fill the brackets</h2>
+          <Grid item>
+          <Typography
+            align="left"
+            color="primary"
+            className={classes.TypographyStyle}
+            style={{ fontSize: 25  }}
+          >
+          Fill the brackets
+          </Typography>
           <AddCountryForm ifDataChanged={props.ifDataChanged} />
-        </Fragment>
+          </Grid>
         )}
+        </Grid>
       </div>
-    </div>
+    
   )
 }
 
